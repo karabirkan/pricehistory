f7fcc263534e17a9c718ea987316d78a1b7433bb
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2024-01-02 18:18:05 +0300

    feat: search bar amazon validation func

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,17 +1,51 @@
 "use client";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
+const isValidAmazonProductUrl = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isValidLink = isValidAmazonProductUrl(searchPrompt);
+    if (!isValidLink) return alert("Please provide a valid Amazon link");
+    try {
+      setIsLoading(true);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         className="searchbar-input"
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter product link"
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}{" "}
       </button>
     </form>
   );
