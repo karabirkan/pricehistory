13964b55a1f1395c1ab529f33717f39227de997f
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2024-01-04 11:52:59 +0300

    feat : price history details page

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -62,7 +62,7 @@ export async function scrapeAmazonProduct(url: string) {
       priceHistory: [],
       discountRate: Number(discountRate),
       category: "category",
-      reviewCount: 100,
+      reviewsCount: 100,
       stars: 4.5,
       isOutOfStock: outOfStock,
       description,
